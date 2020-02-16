6d4e8511407674a141b1c96e6dc1bcd46d4ebc55
parsher <26663229+parsher@users.noreply.github.com>
2020-02-16 20:26:52 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import HomePage from './homepage.component';
+
+class App extends Component {
+  render() {
+    return <HomePage></HomePage>;
+  }
+}
+
+export default App;
