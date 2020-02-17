01442f7c265fd9e46ca53357cab0132f2cbde6b2
parsher <26663229+parsher@users.noreply.github.com>
2020-02-17 21:19:37 +0900

    implemented firebase utils, included ability to store authenticated users with email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,10 +12,38 @@ const config = {
   appId: "1:1006807659595:web:e12647bd50a63f135a49ce"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData)=> {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  
+  if (!snapShot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+//firebase.collection('users').doc('5t39BSdtPt78nXvixnCy').collection('cartItems').doc('HwtrkBK4Uv3fNg5KwuWO');
+//firebase.doc('/users/5t39BSdtPt78nXvixnCy/cartItems/HwtrkBK4Uv3fNg5KwuWO');
+//firebase.collection('/users/5t39BSdtPt78nXvixnCy/cartItems');
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 
