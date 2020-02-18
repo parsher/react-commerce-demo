22bd2407f9d1dc35fcc0a7a4181ad2c6f872c2c5
parsher <26663229+parsher@users.noreply.github.com>
2020-02-18 20:04:11 +0900

    persist localstorage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // localstorage
+
 import cartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
 
-export default combineReducers({
+// whitelist: which do you want to persist
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'] 
+}
+
+const rootReducer =  combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+// persist version reducer
+export default persistReducer(persistConfig, rootReducer);
