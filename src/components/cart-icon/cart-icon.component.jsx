bdc8fbb2d70b2bbd4399b6064ff9dd01b8f5086d
parsher <26663229+parsher@users.noreply.github.com>
2020-02-18 18:14:06 +0900

    change to createStructuredSelector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,9 +4,8 @@ import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.action";
 
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
-
+import { createStructuredSelector } from "reselect";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-
 
 import "./cart-icon.styles.scss";
 
@@ -21,8 +20,8 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = state => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
